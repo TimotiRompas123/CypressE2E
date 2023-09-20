eb8b0535f39a1aaa54f6b428596bcefa6fe6bdbe
timotirompas <timoti.rompas@banksinarmas.com>
2023-09-20 16:31:27 +0700

    first commit

---
+++ cypress/e2e/UAT2/BSIM-IBCDEV-LoginUAT2.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://10.32.1.77:8080/corpibv2/')
+
+    cy.get('#j_customerCode').type('uat')
+    cy.get('#un1').type('uat_maker2')
+    cy.get('#j_password').type('c12345678')
+    cy.get('#proceed').click()
+    cy.contains('Logout').click()
+  })
+})
